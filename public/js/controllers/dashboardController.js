9b64598a182482d18aa9e5e1565f1fb67e225642
HaiDang9719 <haidanglhp1215@gmail.com>
2018-05-07 18:12:23 +0700

    totalRevenue

---
+++ public/js/controllers/dashboardController.js
@@ -0,0 +1,69 @@
+angular.module('dashboardController', [])
+	.controller('mainController',function($scope, $http){
+		$scope.data = [];
+        $scope.item = {};
+        $scope.totalRevenue = 0;
+        $scope.item_qty = 0;
+        $scope.subTotal = 0;
+        $scope.shopName = "shop1";
+        $scope.orderList = {};
+        // $scope.orderList.item=[];
+		// show all the products
+        $http.get('/api/products')
+			.then(function(res){
+/*				console.log(res.data[0].shop1);
+				console.log('x');*/
+                $scope.allProduct = res.data;
+                for(var i=0;i<$scope.allProduct.length;i++)
+                {
+                    for(var j=0; j<$scope.allProduct[i].item.length;j++)
+                    {
+                        $scope.totalRevenue = $scope.totalRevenue+$scope.allProduct[i].item[j].price*$scope.allProduct[i].item[j].quantity_sold;
+                    }
+                }
+                
+
+			})
+			.catch(function(res){
+				//Export error
+                console.log('Error: ' + res);
+                console.log($scope.shopName);
+			})
+			$scope.addFunc = function(){
+                //console.log($scope.item)
+                for(var i=0; i<$scope.orderList.item.length;i++) 
+                {
+                    //console.log($scope.item[i])
+                    if($scope.orderList.item[i].productID == $scope.item_id)
+                    {
+                        $scope.data.push({"id":i,"productID":$scope.item_id,"productName":$scope.orderList.item[i].productName,"price":$scope.orderList.item[i].price,"quantity": $scope.item_qty});
+                        $scope.subTotal = $scope.subTotal + $scope.orderList.item[i].price * $scope.item_qty;
+                        $scope.orderList.item[i].quantity_in_stock = $scope.item[i].quantity_in_stock - $scope.item_qty;
+                        $scope.orderList.item[i].quantity_sold = parseInt($scope.item[i].quantity_sold)+parseInt($scope.item_qty);
+                    }
+
+                }
+                //console.log($scope.idShop);
+            }
+            $scope.payFunc = function(id){
+                $http.put('/api/product/'+$scope.orderList._id,$scope.orderList)
+				.then(function(res){
+                    $scope.abc = res.data;
+                    console.log($scope.orderList)
+                    $scope.data = [];
+                    $scope.item_id = 0;
+                    $scope.item_qty = 0;
+                    $scope.subTotal = 0;
+				})
+				.catch(function(res){
+					//console.log(res.data);
+					console.log("Error" + res)
+				})
+            }
+			
+		
+
+
+	})
+
+
